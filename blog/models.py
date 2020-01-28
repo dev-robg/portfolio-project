e3cfef1e8285614b444ed49a32d32bc96a045ff2
Rob Glasener <robglasener@me.com>
2020-01-28 15:58:24 -0800

    add jobs

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    pubdate = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
